41a556a1548f5c0a57ed0ce19fb58242754753ac
karume-lab <danielkarume@outlook.com>
2026-08-02 10:07:43 +0300

    chore: update decorator plugin configuration to use version property

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,7 @@ module.exports = (api) => {
           },
         },
       ],
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-decorators", { version: "legacy" }],
       "react-native-reanimated/plugin",
     ],
   };
